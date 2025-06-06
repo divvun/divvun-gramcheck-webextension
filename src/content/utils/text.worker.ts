2e2230a0cc080c690f94b9244b04ea36f49473cd
Dylan Hand <dylan.hand@gmail.com>
2025-06-06 10:55:55 +0200

    Remove unused text.worker.ts

--- src/content/utils/text.worker.ts
+++
@@ -1,5 +0,0 @@
-import { clipToErrorContext } from './index';
-
-export async function extractContext(paragraph: string, errorText: string, errorOffset: number): Promise<string> {
-    return clipToErrorContext(paragraph, errorText, errorOffset);
-}
